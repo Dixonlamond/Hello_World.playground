278e4fa478783023b83a2e709e9813b5e250c627
dixonlamond <lamond.dixon@protonmail.com>
2017-06-08 09:04:16 -0600

    First hello world in swift

---
+++ Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// This creating a variable that equals string to "Hello World"
+var str = "Hello World"
+print(str)
+
+// This is just printing out "Hello World" to the console
+print("Hello World")
